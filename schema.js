ec363fa0358bf4132920559cb051d8f52396b31d
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 16:57:49 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -5,7 +5,7 @@ module.exports.listingSchema = Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null),
+        
         location: Joi.string().required(),
         country: Joi.string().required(),
     }).required()
