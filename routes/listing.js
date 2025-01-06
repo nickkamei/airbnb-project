22c8b3e8b2979f5edc06224dda76d0f174512573
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 22:09:28 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -2,38 +2,40 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema} = require("../schema.js");
+const { listingSchema } = require("../schema.js");
 const listingController = require("../controllers/listings.js");
-const multer = require('multer');
-const {storage} = require("../cloudConfig.js");
-const upload = multer({storage});
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-router.route("/")
+router
+    .route("/")
     .get(wrapAsync(listingController.index))
     .post(
-        isLoggedIn, 
+        isLoggedIn,
         upload.single("image"),
-        validateListing, 
+        validateListing,
         wrapAsync(listingController.createListing)
     );
 
-//new route
+// New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-router.route("/:id")
+router
+    .route("/:id")
     .get(wrapAsync(listingController.showListing))
     .put(
-        isLoggedIn, 
-        isOwner, 
+        isLoggedIn,
+        isOwner,
         upload.single("image"),
-        validateListing, 
+        validateListing,
         wrapAsync(listingController.updateListing)
     )
     .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-//edit route
+// Edit route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
