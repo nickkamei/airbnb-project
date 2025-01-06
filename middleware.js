5fb978367dcf1649c3dc5ec89bf9ce313e9569e7
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 21:20:10 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -32,6 +32,7 @@ module.exports.isOwner = async (req, res, next) => {
 
 //validatelisting function
 module.exports.validateListing = (req, res, next) => {
+    console.log("Validating listing...");
     let { error } = listingSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
@@ -40,6 +41,7 @@ module.exports.validateListing = (req, res, next) => {
     }
     next();
 };
+
 
 module.exports.validateReview = (req, res, next) => {
     let {error} = reviewSchema.validate(req.body);
