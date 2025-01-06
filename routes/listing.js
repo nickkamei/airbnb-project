8cbd385e840f108413ae9720805bc51f67d62e99
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 18:09:18 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -9,6 +9,7 @@ const listingController = require("../controllers/listings.js");
 const multer  = require('multer');
 const {storage} = require("../cloudConfig.js");
 const upload = multer({storage});
+const { wrapAsync } = require("../utils/wrapAsync.js");
 
 const wrapAsync = (fn) => {
     return function (req, res, next) {
