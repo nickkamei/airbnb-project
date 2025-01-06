7d9aa82a337f3e5dfa228a9e9c2094b8dd78816c
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 22:14:00 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,9 +4,8 @@ const {listingSchema, reviewSchema} = require("./schema.js");
 const ExpressError = require("./utils/ExpressError.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
-        req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "you must be logged in to create listing!");
+    if (!req.isAuthenticated()) {
+        req.flash("error", "You must be signed in!");
         return res.redirect("/login");
     }
     next();
@@ -20,11 +19,11 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 };
 
 module.exports.isOwner = async (req, res, next) => {
-    const listingId = req.params.id;
-    let listing = await Listing.findById(listingId);
-    if(!listing.owner._id.equals(res.locals.currUser._id)) {
-        req.flash("error", "You are not the owner of this listing");
-        return res.redirect(`/listings/${listingId}`);
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing.owner.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that!");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 };
@@ -32,16 +31,13 @@ module.exports.isOwner = async (req, res, next) => {
 
 //validatelisting function
 module.exports.validateListing = (req, res, next) => {
-    console.log("Validating listing...");
-    let { error } = listingSchema.validate(req.body);
+    const { error } = listingSchema.validate(req.body);
     if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        req.flash("error", errMsg);
-        return res.redirect("/listings/new");
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
     }
     next();
 };
-
 
 module.exports.validateReview = (req, res, next) => {
     let {error} = reviewSchema.validate(req.body);
