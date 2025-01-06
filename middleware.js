716764b0fffb61480d93ad4d4479e2551a5173e6
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 14:42:59 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -31,10 +31,6 @@ module.exports.isOwner = async (req, res, next) => {
 
 
 //validatelisting function
-const { listingSchema } = require("./schema.js");
-const ExpressError = require("./utils/ExpressError.js");
-const Listing = require("./models/listing.js");
-
 module.exports.validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     if (error) {
