89c8adba977c9315f46693878b215d35b9a4deb4
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 14:13:39 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -36,24 +36,41 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-    let response = await geocodingClient.forwardGeocode({
-        query: req.body.listing.location,
-        limit: 1
-      })
-        .send();
-    let url = req.file.path;
-    let filename = req.file.filename;
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = {url, filename};
-    
-    // Extract geometry correctly
-    newListing.geometry = response.body.features[0].geometry;
-    
-    let savedListing = await newListing.save();
-    console.log(savedListing);
-    req.flash("success", "New Listing Created!");
-    res.redirect("/listings");
+    try {
+        // Check if file exists
+        if (!req.file) {
+            req.flash("error", "Please upload an image");
+            return res.redirect("/listings/new");
+        }
+
+        // Geocode the location
+        let response = await geocodingClient.forwardGeocode({
+            query: req.body.listing.location,
+            limit: 1
+        }).send();
+
+        let url = req.file.path;
+        let filename = req.file.filename;
+        const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
+        newListing.image = {url, filename};
+        
+        // Add error check for geocoding response
+        if (!response.body.features[0]) {
+            req.flash("error", "Location not found");
+            return res.redirect("/listings/new");
+        }
+        newListing.geometry = response.body.features[0].geometry;
+        
+        let savedListing = await newListing.save();
+        console.log(savedListing);
+        req.flash("success", "New Listing Created!");
+        res.redirect("/listings");
+    } catch (error) {
+        console.error("Error creating listing:", error);
+        req.flash("error", "Error creating listing");
+        return res.redirect("/listings/new");
+    }
 };
 
 module.exports.renderEditForm = async (req, res) => {
