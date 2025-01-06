bd6a10622700fe5d9f87d4274d6482f6d1c2977d
nickkamei <61488639+nickkamei@users.noreply.github.com>
2025-01-06 22:11:07 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,24 +1,23 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUD_NAME || "default_cloud_name",
+    api_key: process.env.CLOUD_API_KEY || "default_api_key",
+    api_secret: process.env.CLOUD_API_SECRET || "default_api_secret",
 });
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
-      allowerFormats: async (req, file) => ["png","jpg","jpeg"],
+        folder: "wanderlust_DEV",
+        allowed_formats: ["png", "jpg", "jpeg"],
     },
-  });
+});
 
-  module.exports = {
+console.log("Cloudinary config:", cloudinary.config());
+
+module.exports = {
     cloudinary,
-    storage
-  }
-
-
-console.log('Cloudinary config:', cloudinary.config());
+    storage,
+};
